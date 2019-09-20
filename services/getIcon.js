939a9b8c3fad82a9d95e797e00fb043229b3d682
Akshay Kumar Sharma <akzarma@gmail.com>
2019-09-20 17:50:43 +0530

    UI fixes in push notification

--- services/getIcon.js
+++ services/getIcon.js
@@ -1,6 +1,6 @@
 const getIcon = {
-  'User Joined': 'png/joined-user.png',
-  'User Left': 'png/joined-left.png',
+  'User Joined': '/png/joined-user.png',
+  'User Left': '/png/joined-left.png',
   'Broadcast Started': '/png/broadcast-start.png',
   'Broadcast Stopped': '/png/broadcast-stop.png',
   'Other Connection Issue': '/png/connection-issue.png',
